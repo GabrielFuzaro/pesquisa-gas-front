62d3cfc669e8f771b25818dfd015c12cc67c1afd
Gabriel Fuzaro <gabrielbbfuzaro@gmail.com>
2026-09-22 16:45:36 +0000

    Adiciona método para criar novos tamanhos de botijão
    Complementa o serviço de tamanhos com a criação via POST, já que a
    lista deixou de ser fixa e passa a ser alimentada por quem cadastra
    os preços.

--- src/app/routes/tamanho-botijao.service.ts
+++ src/app/routes/tamanho-botijao.service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { CookieService } from "../services/cookie.service";
 import { TamanhoBotijao } from "../interfaces/dto/tamanho-botijao";
+import { TamanhoBotijaoInput } from "../interfaces/input/tamanho-botijao-input";
 
 @Injectable({
     providedIn: 'root',
@@ -25,6 +26,14 @@ export class TamanhoBotijaoService {
 
     listarTamanhos(): Observable<TamanhoBotijao[]> {
         return this.http.get<TamanhoBotijao[]>(`${this.apiTamanho}`,
+            {
+                headers: this.getHeaders()
+            }
+        )
+    }
+
+    criarTamanho(input: TamanhoBotijaoInput): Observable<TamanhoBotijao> {
+        return this.http.post<TamanhoBotijao>(`${this.apiTamanho}`, input,
             {
                 headers: this.getHeaders()
             }
